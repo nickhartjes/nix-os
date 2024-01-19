c3aa543d9995df322c52a4ba6e88b906a00a3aff
Nick Hartjes <info@nickhartjes.nl>
2024-01-19 14:23:23 +0100

    Added keys

--- nixos/modules/terminal/ssh.nix
+++ nixos/modules/terminal/ssh.nix
@@ -16,4 +16,7 @@
 
   home.file.".ssh/id_rsa_ubuntu".source = ./source/id_rsa_ubuntu;
   home.file.".ssh/id_rsa_ubuntu.pub".source = ./source/id_rsa_ubuntu.pub;
+
+  home.file.".ssh/id_ed25519_thinkpad_t15".source = ./source/id_ed25519_thinkpad_t15;
+  home.file.".ssh/id_ed25519_thinkpad_t15.pub".source = ./source/id_ed25519_thinkpad_t15.pub;
 }
