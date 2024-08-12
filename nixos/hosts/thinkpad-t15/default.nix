232e8db0e3d5d425f2b2435206e50b6aa8bb5774
Nick Hartjes <info@nickhartjes.nl>
2024-08-12 15:32:46 +0200

    Added new displaylink Drivers

--- nixos/hosts/thinkpad-t15/default.nix
+++ nixos/hosts/thinkpad-t15/default.nix
@@ -6,7 +6,7 @@
     [(import ../../modules/programs/display-managers/greetd.nix)] ++
     # [(import ../../modules/desktop/awesome/default.nix)] ++
     [(import ../../modules/desktop/plasma/default.nix)] ++
-   # [(import ../../modules/desktop/cosmic/default.nix)] ++
+    [(import ../../modules/desktop/cosmic/default.nix)] ++
     [(import ../../modules/desktop/sway/default.nix)] ++
     (import ../../modules/desktop/virtualisation) ++
     [(import ../../modules/hardware/displaylink.nix)] ++
@@ -19,7 +19,7 @@
     # Bootloader.
     boot.loader.systemd-boot.enable = true;
     boot.loader.efi.canTouchEfiVariables = true;
-  
+
     ##################
     ## Hardware specific sytem settings
     ##################
